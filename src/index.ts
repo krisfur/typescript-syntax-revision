526913ff986224e7b7223b295afe720204e33853
Krzysztof Furman <k_furman@outlook.com>
2025-10-14 11:09:29 +0100

    Initial push.

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+console.log('TypeScript Syntax Revision Notes')
+
+//console.log("\n");
+console.log('============')
+console.log('VARIABLES')
+console.log('============')
+// Variables
+const namedString: string = 'This is a const string';
+// types allowed: string, number, boolean, any, void, null, undefined, never
+
+console.log(`Formatted string with variable: ${namedString}`);
+let count: number = 0;
+console.log(`Your count of type ${typeof count} is ${count}`);
+count += 1;
+console.log(`Your updated count of type ${typeof count} is ${count}`);
+
